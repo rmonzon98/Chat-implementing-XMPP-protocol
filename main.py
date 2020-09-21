64187fb49676dd300d925efb3b141c04fcc8d98a
Raul Monzon <rmonzon1998@gmail.com>
2020-09-20 21:55:06 -0600

    Se crea opción nueva en el menu
    Para facilitar el uso, se crea una nueva opción en el menu. Ahora entrar/crear grupo esta separado de la opción de enviar mensaje a grupo.

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ if __name__ == '__main__':
     5. Agregar usuario como contacto
     6. Mostrar detalles de una cuenta
     7. Enviar mensaje (Mensaje directo)
-    8. Enviar mensaje (Mensaje de grupo)
-    9. Definir mensaje de presencia
+    8. Ingresar a un nuevo grupo
+    9. Enviar mensaje (Mensaje de grupo)
+    10. Definir mensaje de presencia
     99. Salir
     """
 
@@ -111,35 +112,32 @@ if __name__ == '__main__':
             else:
                 print('No ha iniciado sesion')
 
-        #Enviar mensaje (Mensaje grupal)
+        #Entrar/crear grupo
         elif opcion == '8':
             print ('\n*************opcion 8*************\nEnviar mensaje (Mensaje grupal)')
             if login:
-                if group_joined:
-                    print('Pertenece a un grupo, puede enviar un mensaje')
-                    new_group = input('¿Desea ingresar a uno nuevo? \n1. si \n2. no')
-
-                    if new_group == '1':
-                        room = input("Ingrese el room al que desee entrar. Si este existe, se creara uno nuevo con ese nombre.\nnombre:")
-                        nickname = input ("Ingrese el nickname con el que desea presentarse")
-                        cliente.createRoom(room+'@conference.redes2020.xyz',nickname)
-                    elif new_group == '2':
-                        room = input("Ingrese el room al que desee entrar: ")
-                        msg = input("Ingrese el mensaje que desea enviarle al grupo:\n")
-                        cliente.send_Msg_group(room+'@conference.redes2020.xyz',message)                    
-                else:
-                    print('Aun no pertenece a ningun grupo, entre a uno primero')
-                    room = input("Ingrese el room al que desee entrar. Si este existe, se creara uno nuevo con ese nombre.\nnombre:")
-                    nickname = input ("Ingrese el nickname con el que desea presentarse")
-                    cliente.createRoom(room+'@conference.redes2020.xyz',nickname)
-                    group_joined = True
-
+                room = input("Ingrese el room al que desee entrar. Si este no existe, se creara uno nuevo con ese nombre.\nnombre: ")
+                nickname = input ("Ingrese el nickname con el que desea presentarse: ")
+                cliente.createRoom(room+'@conference.redes2020.xyz',nickname)
+                group_joined = True
             else:
                 print('No ha iniciado sesion')
 
-        #Definir mensaje de presencia
+        #Enviar mensaje (Mensaje grupal)
         elif opcion == '9':
-            print ('\n*************opcion 9*************\nDefinir mensaje de presencia')
+            if login: 
+                if group_joined:
+                    room = input("Ingrese el room al que desee entrar: ")
+                    msg = input("Ingrese el mensaje que desea enviarle al grupo:\n")
+                    cliente.send_Msg_group(room+'@conference.redes2020.xyz',message)                    
+                else:
+                    print('Aun no pertenece a ningun grupo, entre a uno primero')
+            else:
+                print("No ha iniciado sesion")
+
+        #Definir mensaje de presencia
+        elif opcion == '10':
+            print ('\n*************opcion 10*************\nDefinir mensaje de presencia')
             if login:
                 status = input('Elija su estado \n1. away\n2. chat\n3. xa\n 4. dnd\n')
                 status=int(status)
